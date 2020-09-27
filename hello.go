fd3d65a3e048f05d3e7e2607896a5e769d1cd31a
Tatiana Moreno <tatiana.emilia.morenno@gmail.com>
2020-09-27 18:41:13 -0300

    arrays and slices in go

--- hello.go
+++ hello.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"reflect"
 )
 
 func main() {
@@ -19,9 +20,18 @@ func main() {
 	// } else {
 	// 	fmt.Println("Não conheço este comando")
 	// }
-	exibeIntroducao()
+	// exibeIntroducao()
+
+	var sites [4]string
+	sites[0] = "https://random-status-code.herokuapp.com"
+	sites[1] = "https://alura.com.br"
+	sites[2] = "https://google.com.br"
+	fmt.Println(reflect.TypeOf(sites))
+	fmt.Println(sites)
+	exibeNomes()
+
 	for {
-		exibeMenu()
+		// exibeMenu()
 		comando := leComando()
 
 		switch comando {
@@ -55,13 +65,18 @@ func leComando() int {
 	var comandoLido int
 	fmt.Scan(&comandoLido)
 	fmt.Println("O comando escolhido foi: ", comandoLido)
-
 	return comandoLido
 }
 
 func iniciarMonitoramento() {
 	fmt.Println("Monitorando...")
-	site := "https://random-status-code.herokuapp.com"
+	var sites [4]string
+	sites[0] = "https://random-status-code.herokuapp.com"
+	sites[1] = "https://alura.com.br"
+	sites[2] = "https://google.com.br"
+	fmt.Println(sites)
+
+	site := "https://random-status-code.herokuapp.com/"
 	resp, _ := http.Get(site)
 
 	if resp.StatusCode == 200 {
@@ -69,6 +84,21 @@ func iniciarMonitoramento() {
 	} else {
 		fmt.Println("Site: ", site, " está com problemas. Status Code: ", resp.StatusCode)
 	}
+}
+
+func exibeNomes() {
+	nomes := []string{"Tatiana", "Anderson", "Luiz"}
+	fmt.Println(nomes)
+	fmt.Println(reflect.TypeOf(nomes))
+	fmt.Println("O meu slice tem: ", len(nomes), "itens")
+	fmt.Println("O meu slice tem capacidade para: ", cap(nomes), "itens")
+
+	nomes = append(nomes, "Moreno")
+
+	fmt.Println(nomes)
+	fmt.Println(reflect.TypeOf(nomes))
+	fmt.Println("O meu slice tem: ", len(nomes))
+	fmt.Println("O meu slice tem capacidade para: ", cap(nomes), "itens")
 }
 
 func exibirLogs() {
